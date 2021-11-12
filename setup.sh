18ebd201d8402c85c87b271bd6bc3cae2d8bbdf2
jo-aguilar <joaoaguilar93@gmail.com>
2021-11-12 12:53:04 -0300

    scripts atualizados

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo apt-get -y update --allow-releaseinfo-change
+sudo apt-get install -y vim
+sudo apt-get install -y build-essential
+sudo apt-get install -y tty-clock
+sudo apt-get install -y tmux
+sudo apt-get install -y git
+sudo apt-get install -y rpl
+HOME=~
+DIR="/conio.h/"
+if [ ! -d "$HOME$DIR" ]
+    git clone https://github.com/zoelabbb/conio.h
+    sudo scp ~/conio.h/conio.h /usr/include/
+fi
---
+++ update.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+#script para manutenção do sistema
+sudo apt-get -y update 
+sudo apt-get -y upgrade 
+sudo apt-get -y autoremove
