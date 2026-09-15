54e1168d77aa854d4b0ac09cd3ed454bcf85cf44
jo-aguilar <joaoaguilar93@gmail.com>
2026-09-15 10:02:16 -0300

    setup atualizado

--- setup.sh
+++ setup.sh
@@ -9,6 +9,24 @@ sudo apt-get install -y tmux
 sudo apt-get install -y git
 sudo apt-get install -y rpl
 sudo apt-get install -y gdb
+sudo apt-get install -y sox libsox-fmt-all
+sudo apt-get install -y alacritty
+sudo apt-get install -y pulseaudio-utils
+
+# 1. Cria os diretórios necessários em ~/.config
+mkdir -p "$HOME/.config/alacritty/sounds"
+
+# 2. Gera o arquivo de áudio (450 Hz, 150 ms, fade para evitar estalos)
+SOX_OUT="$HOME/.config/alacritty/sounds/beep_450.wav"
+sox -n -r 44100 -c 1 "$SOX_OUT" synth 0.150 sine 450 fade h 0.005 0.150 0.025 vol 0.7
+
+# 3. Escreve a configuração no alacritty.toml
+cat <<EOF > "$HOME/.config/alacritty/alacritty.toml"
+[bell]
+animation = "Off"
+duration = 0
+command = { program = "/usr/bin/paplay", args = ["$SOX_OUT"] }
+EOF
 
 #instalar rakudo
 mkdir ~/rakudo && cd $_ &&
